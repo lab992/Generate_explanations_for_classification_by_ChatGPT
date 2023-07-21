31a7590b84ad5c5384791609a3b6aa18e735275a
lab992 <61621208+lab992@users.noreply.github.com>
2023-07-21 17:14:22 +0200

    feat: implement IO and gpt

---
+++ util/GPTExecutor.py
@@ -0,0 +1,47 @@
+import openai
+import time
+
+def judge_correctness():
+    pass
+
+class GPTExecutor:
+
+    def __init__(self, context, query):
+        self.context = context
+        self.query = query
+
+
+    def gpt_execution(context, query):
+        openai.api_key = "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"
+
+        count = 0
+
+        run_times = 15
+
+        while (count < run_times):
+            try:
+                completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": context},
+                        {"role": "user", "content": query},
+                    ]
+                )
+
+                gpt_result = completion.choices[0].message["content"]
+
+                filename = "explain.txt"
+
+                with open(filename, "a") as file:
+                    file.write(', '.join(map(str, gpt_result)))
+                    file.write("\n")
+                
+                count += 1
+
+                time.sleep(25)
+            except ValueError:
+                print("error")
+                pass
+            except Exception as e:
+                raise e
+        return
