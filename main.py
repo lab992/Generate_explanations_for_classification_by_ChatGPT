9849ebae85586d3762fd71a2c065c3f52bb30759
lab992 <61621208+lab992@users.noreply.github.com>
2023-11-12 20:02:33 +0100

    style: sort files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from util import FileReader, FeatureSelection
 from util import FileReader, FeatureSelection, GPTExecutor, DecisionTree, ContextGenerator, QueryTransformer
+
 def print_query(query):
      for i in range(len(query)):
         with open("0.04_query_50.txt", "a") as file:
@@ -21,15 +22,18 @@ def pipeline():
     FEATURE_AMOUNT = 3
     train_set, test_set = FeatureSelection.feature_selection(X_train, X_test, y_train, FEATURE_AMOUNT)
     
+    # test accuracy of decision tree
     # DecisionTree.graph_and_accuracy(train_set, test_set, y_train, y_test)
 
     context, lookup_table = ContextGenerator.gen_context("acc", train_set, y_train)
 
     query = QueryTransformer.gen_query(test_set, lookup_table)
 
-    print_query(query.to_list())
+    # robustness
+    # print_query(query.to_list())
     # print_context(context)
-    # GPTExecutor.gpt_execution(context, query)
+
+    GPTExecutor.gpt_execution(context, query)
 
 if __name__ == "__main__":
     pipeline()
